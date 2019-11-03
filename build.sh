d4a121e0ad1bfaf4b7e6d0862250794506b7968a
Victor Sanchez <incomingvrs@gmail.com>
2019-11-03 13:14:46 -0800

    Ironed out link problems

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-cat templates/top.html contents/about.html templates/bottom.html > docs/about_page_built.html
+cat templates/top.html contents/about.html templates/bottom.html > docs/about.html
+cat templates/top.html contents/projects.html templates/bottom.html > docs/projects.html
+cat templates/top.html contents/index.html templates/bottom.html > docs/index.html
