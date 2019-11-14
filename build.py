f550bdb388ba2dacbd7c3ba90a0a306cdc4a9797
Victor Sanchez <incomingvrs@gmail.com>
2019-11-13 20:05:05 -0800

    replaced original build file

--- build.py
+++ build.py
@@ -1,15 +1,50 @@
-# Templates top and bottom
-top = open('templates/top.html').read()
-bottom = open('templates/bottom.html').read()
+def main():    
+    for page in pages:
+        filename = page["filename"]
+        output = page["output"]
+        title = page["title"] 
 
-# Contents 
-about_content = open('content/about.html').read()
-projects_content = open('content/projects.html').read()
-index_content = open('content/index.html').read()
+        content = read_file(filename) 
+        template = apply_template(content, title)
+        # Write webpage
+        write_page(template, output)
+
+# Generates individual pages, inserting page content and title
+def apply_template(content, title):
+    template = open("templates/base.html").read()
+    template = template.replace("{{content}}", content)
+    template = template.replace("{{title}}", title)
+    return template
 
 
+# Writes generated pages into end files
+def write_page(template, output):
+    open(output, "w+").write(template)
+    return 
 
-# Creates html files with "top" + "content" + "bottom"
-open('docs/about.html', 'w+').write(top + about_content + bottom)
-open('docs/projects.html', 'w+').write(top + projects_content + bottom)
-open('docs/index.html', 'w+').write(top + index_content + bottom)
+def read_file(filename):
+    content = open(filename).read()
+    return content
+
+
+pages = [
+    {
+    "filename": "content/about.html",
+    "output": "docs/about.html",
+    "title": "About Me",
+    },
+    {
+    "filename": "content/projects.html",
+    "output": "docs/projects.html",
+    "title": "My Projects",
+    },
+    {
+    "filename": "content/index.html",
+    "output": "docs/index.html",
+    "title": "My programming blog",
+    },
+]
+
+if __name__ == "__main__":
+    main()
+
